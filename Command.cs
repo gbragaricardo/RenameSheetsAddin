1a39436d1472651ac3dab80d692669ea6b6726a3
Ricardo Garcia Braga <ricardobriangarcia@gmail.com>
2026-08-28 13:13:53 -0300

    Implement Revit command and update project/addin config
    Add IExternalCommand implementation in Command.cs (stub Execute returning Result.Succeeded) and include Revit API usings. Update RenameSheets.Addin.csproj to set LangVersion, UseWPF, ImportWindowsDesktopTargets, PlatformTarget=x64, AppendTargetFrameworkToOutputPath, and preserve RevitYear/OutputPath and package reference. Modify RenameSheets.addin to point Assembly to RenameSheets/RenameSheets.Addin.dll.

--- Command.cs
+++ Command.cs
@@ -1,6 +1,12 @@
-﻿namespace RenameSheets.Addin;
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
 
-public class Command
+namespace RenameSheets.Addin;
+
+public class Command : IExternalCommand
 {
-
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        return Result.Succeeded;
+    }
 }
